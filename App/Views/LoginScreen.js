60723501427dc6fbf1debbc019ae585f76caf931
Daniel Vorster <VRSDAN004@myuct.ac.za>
2019-08-27 11:26:40 +0200

    Login Screen using stylesheet

--- App/Views/LoginScreen.js
+++ App/Views/LoginScreen.js
@@ -10,6 +10,7 @@
 
 import React, { Component } from 'react';
 import LoginController from '../Controllers/LoginController'
+import styles from '../Styles/styles'
 import {
     StyleSheet,
     View,
@@ -21,7 +22,6 @@ import {
   } from 'react-native';
   
 import AsyncStorage from '@react-native-community/async-storage';
-const {width: WIDTH} = Dimensions.get('window') //Window width for formatting
 
 export default class LoginScreen extends Component {
   constructor(props) {
@@ -91,72 +91,3 @@ export default class LoginScreen extends Component {
     }
 }
 
-const styles = StyleSheet.create({ //Styles
-    container: 
-    {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#4682b4',
-    },
-    engine: {
-      position: 'absolute',
-      right: 0,
-    },
-    sectionContainer: {
-      marginTop: 32,
-      paddingHorizontal: 24,
-    },
-    sectionTest: 
-    {
-      fontSize: 50,
-      fontWeight: '600',
-      color: "purple",
-      textAlign: "center",
-    },
-    headingText: 
-    {
-        color: '#ffffff',
-        fontSize: 40,
-        fontWeight: 'bold',
-        marginTop: 10,
-        opacity: 0.5,
-    },
-    input: 
-    {
-      width: WIDTH - 55,
-      height: 45,
-      borderRadius: 0,
-      fontSize: 16,
-      paddingLeft: 45,
-      backgroundColor: '#add8e6',
-      fontWeight: 'bold',
-      color: '#ffffff',
-      marginHorizontal: 25,
-      opacity: 0.6
-    },
-    inputContainer: 
-    {
-        marginTop: 10,
-    },
-    btnLogin: 
-    {
-        width: WIDTH - 150,
-        height: 45,
-        borderRadius: 0,
-        fontSize: 16,
-        marginTop: 25,
-        backgroundColor: '#add8e6',
-        color: '#ffffff',
-        justifyContent: 'center',
-
-    },
-    loginText: 
-    {
-        color: '#ffffff',
-        fontSize: 20,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        opacity: 0.6
-    }
-  });
