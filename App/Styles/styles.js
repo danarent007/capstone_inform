c29edd6df800acd5ee238a85d8dc607efebe9f1e
Daniel Vorster <VRSDAN004@myuct.ac.za>
2019-08-27 12:12:23 +0200

    (most) Views now using sytle sheet

--- App/Styles/styles.js
+++ App/Styles/styles.js
@@ -2,6 +2,9 @@ import { StyleSheet } from "react-native"
 import {
   Dimensions
 } from 'react-native';
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
 
 const {width: WIDTH} = Dimensions.get('window')
 
@@ -55,15 +58,19 @@ export default StyleSheet.create({ //Styles
     },
     btnLogin: 
     {
-        width: WIDTH - 150,
-        height: 45,
-        borderRadius: 0,
-        fontSize: 16,
-        marginTop: 25,
-        backgroundColor: '#add8e6',
-        color: '#ffffff',
-        justifyContent: 'center',
-
+      width: WIDTH - 150,
+      height: 45,
+      borderRadius: 0,
+      fontSize: 16,
+      marginTop: 25,
+      backgroundColor: '#add8e6',
+      color: '#ffffff',
+      justifyContent: 'center',
+      shadowOffset: {width: 0, height: 2}, 
+      elevation: 5, 
+      shadowColor: '#000', 
+      shadowOpacity: 0.25, 
+      shadowRadius: 3.84
     },
     loginText: 
     {
@@ -72,5 +79,79 @@ export default StyleSheet.create({ //Styles
         textAlign: 'center',
         fontWeight: 'bold',
         opacity: 0.6
+    },
+    scrollView: {
+      backgroundColor: Colors.lighter,
+    },
+    engine: {
+      position: 'absolute',
+      right: 0,
+    },
+    body: {
+      backgroundColor: Colors.white,
+    },
+    sectionContainer: {
+      marginTop: 32,
+      paddingHorizontal: 24,
+    },
+    sectionTest: 
+    {
+      fontSize: 30,
+      fontWeight: '600',
+      color: "white",
+      textAlign: "center",
+      width: 200,
+      alignSelf: 'center'
+    },
+    header: 
+    {
+      width: WIDTH,
+      height: 45,
+      backgroundColor: "#4682b4",
+      flexDirection: 'column',
+      fontWeight: "bold"
+    },
+    postButton: 
+    {
+     backgroundColor: '#a302a3',
+     height: 60,
+     width: 50,
+     alignSelf: 'flex-end',
+    
+    },
+    image:
+    {
+      zIndex: 1,
+      height: 50,
+      alignSelf: 'flex-end',
+      width: 60,
+      tintColor: '#610061',
+      position: 'absolute'
+     
+    },
+    creatPostFloatButton:
+    {
+      borderWidth:0,
+      borderColor:'rgba(0,0,0,0.2)',
+      alignItems:'center',
+      justifyContent:'center',
+      width:70,
+      position: 'absolute',                                          
+      bottom: 10,                                                    
+      right: 10,
+      height:70,
+      backgroundColor:'#add8e6',
+      borderRadius:100,
+      shadowOffset: {width: 0, height: 2}, 
+      elevation: 5, 
+      shadowColor: '#000', 
+      shadowOpacity: 0.25, 
+      shadowRadius: 3.84
+    },
+    pfeed:
+    {
+      width: WIDTH,
+      alignItems:'center', 
+      backgroundColor: '#add8e6'
     }
   });
