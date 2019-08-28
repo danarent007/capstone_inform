f5eac0be273f62b174473047daa137738a0333d5
Daniel Vorster <VRSDAN004@myuct.ac.za>
2019-08-28 14:04:58 +0200

    Renamed CustomPost and CustomListView

--- App/Views/CustomPost.js
+++ App/Views/CustomPost.js
@@ -5,6 +5,7 @@
 ** Sheldon Reay (RXYSHE002)
 ** Sabir Buxsoo (BXSMUH001)
 ** Daniel Vorster (VRSDAN004)
+** Post.js
 */
 
 import React from 'react';
@@ -13,7 +14,7 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 const PHOTO_URL = 'https://avatars2.githubusercontent.com/u/26744195?s=400&u=b15b3c68813c1f17313a79c49691800b61a837b4&v=4'
 
 
-const CustomRow = ({ title, description, image_url }) => (
+const CustomPost = ({ title, description, image_url }) => (
     <View style={styles.post_container}>
         <Image source={{ uri: PHOTO_URL }} style={styles.post_photo} />
         <View style={styles.post_container_text}>
@@ -28,4 +29,4 @@ const CustomRow = ({ title, description, image_url }) => (
     </View>
 );
 
-export default CustomRow;
+export default CustomPost;
